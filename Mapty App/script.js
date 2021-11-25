5ce2f37d50d43a150f5ef8bfebb1624900b886ca
advaitvd <advaitvdgoa@gmail.com>
2021-11-26 03:04:26 +0530

    map added with popups

--- Mapty App/script.js
+++ Mapty App/script.js
@@ -10,3 +10,48 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude, longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
+
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent.latlng);
+        const { lat, lng } = mapEvent.latlng;
+        const coord = [lat, lng];
+        L.marker(coord)
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: '250px',
+              minWidth: '100px',
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
+    },
+    function () {
+      alert('Could not get your location.');
+    }
+  );
+}
